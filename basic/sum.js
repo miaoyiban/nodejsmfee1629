09751f49dae71abe7b43c7214c4fcd39c466ee0c
kun <fdsa0872@gmail.com>
2021-05-22 16:37:08 +0800

    排版

--- basic/sum.js
+++ basic/sum.js
@@ -1,11 +1,13 @@
 console.log("Hello");
-function sum(n){
-    
-    let total = n*(n+1)/2;
 
-    return total
+function sum(n) {
+	let total = (n * (n + 1)) / 2;
+
+	return total;
 }
 console.log(sum(1)); // 1
 console.log(sum(2)); // 3
 console.log(sum(10)); // 55
 console.log(sum(100000)); // 5000050000
+
+
