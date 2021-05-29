9aff89fe0425d2225c47902e623149c725bb8860
kun <fdsa0872@gmail.com>
2021-05-29 12:56:05 +0800

    modify

--- crawler/app.js
+++ crawler/app.js
@@ -4,3 +4,20 @@
 //&stockNo=2330
 //&_=1621756492292
 
+const axios = require('axios');
+axios
+	.get(
+		"https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210529&stockNo=2330"
+	)
+	.then(function (response) {
+		// handle success
+		console.log(response.data.data);
+        
+	})
+	.catch(function (error) {
+		// handle error
+		console.log(error);
+	})
+	.then(function () {
+		// always executed
+	});
