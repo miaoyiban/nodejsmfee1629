a40a7bbc8f0c66429787f9267d760644413ea5c0
kun <fdsa0872@gmail.com>
2021-05-30 14:49:36 +0800

    use await

---
+++ crawler/app-await.js
@@ -0,0 +1,53 @@
+//https://www.twse.com.tw/exchangeReport/STOCK_DAY
+//?response=json
+//&date=20210523
+//&stockNo=2330
+//&_=1621756492292
+
+const { rejects } = require("assert");
+const axios = require("axios");
+//fs=filesystem，不用安裝node內建
+
+function rFPromise() {
+	return new Promise((resolve, reject) => {
+		const fs = require("fs");
+		fs.readFile("stock.txt", "utf8", (err, data) => {
+			if (err) {
+				reject(err);
+			}
+			resolve(data);
+		});
+	});
+}
+
+let date = new Date();
+// console.log(date);
+let year = date.getFullYear();
+// console.log(year)
+let month = (date.getMonth() + 1 < 10 ? "0" : "") + (date.getMonth() + 1);
+// console.log(month);
+let day = (date.getDate() + 1 < 10 ? "0" : "") + date.getDate();
+// console.log(day)
+let fullDate = year + month + day;
+// console.log(fullDate)
+
+async function clawer() {
+	try {
+		let rfResult = await rFPromise();
+		let axResult = await axios.get(
+			"https://www.twse.com.tw/exchangeReport/STOCK_DAY",
+			{
+				params: {
+					response: "json",
+					date: "20210529",
+					stockNo: rfResult,
+				},
+			}
+		);
+        console.log(axResult.data.title)
+	} catch (err) {
+		console.log(err);
+	}
+}
+clawer();
+
