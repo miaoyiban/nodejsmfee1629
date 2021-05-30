2225bd1dc4479ed7956098e26cf39f73da456d5a
kun <fdsa0872@gmail.com>
2021-05-30 12:29:13 +0800

    改promise

--- crawler/app.js
+++ crawler/app.js
@@ -5,19 +5,32 @@
 //&_=1621756492292
 
 const axios = require('axios');
-axios
-	.get(
-		"https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210529&stockNo=2330"
-	)
-	.then(function (response) {
-		// handle success
-		console.log(response.data.data);
-        
-	})
-	.catch(function (error) {
-		// handle error
-		console.log(error);
-	})
-	.then(function () {
-		// always executed
-	});
+//fs=filesystem，不用安裝node內建
+const fs = require("fs");
+fs.readFile("stock.txt","utf8",(err,data)=>{
+	if(err){
+		return console.error("讀檔錯誤",err)
+	}
+	console.log(`讀到的StockCode ${data}`)
+
+	axios
+		.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+			params: {
+				response: "json",
+				date: "20210529",
+				stockNo: data,
+			},
+		})
+		.then(function (response) {
+			// handle success
+			console.log(response.data.title);
+		})
+		.catch(function (error) {
+			// handle error
+			console.log(error);
+		})
+		.then(function () {
+			// always executed
+		});
+});
+// const stockCode =???
