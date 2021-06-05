3e9d31959dbcf4272a38f249e3598357087c5e2b
kun <fdsa0872@gmail.com>
2021-06-05 11:20:32 +0800

    bluebird

---
+++ crawler/app-bluebird.js
@@ -0,0 +1,46 @@
+const axios = require("axios");
+const fs = require("fs");
+const moment = require("moment");
+const Promise = require("bluebird");
+// console.log(Promise)
+
+// console.log(moment().format()); // 2021-05-30T13:45:06+08:00
+// console.log(moment().format("YYYYMMDD")); // 20210530
+
+// 因為用bluebird所以不用自己包
+// function readFilePromise() {
+// 	return new Promise((resolve, reject) => {
+// 		fs.readFile("stock.txt", "utf8", (err, data) => {
+// 			if (err) {
+// 				reject(err);
+// 			}
+// 			resolve(data);
+// 		});
+// 	});
+// }
+
+// 用bluebird 包 callback版本的readFile
+
+const readFileBlue = Promise.promisify(fs.readFile)
+
+readFileBlue("stock.txt","utf-8")
+	.then((stockCode) => {
+		console.log("stockCode:", stockCode);
+
+		return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+			params: {
+				response: "json",
+				date: moment().format("YYYYMMDD"),
+				stockNo: stockCode,
+			},
+		});
+	})
+	.then((response) => {
+		if (response.data.stat === "OK") {
+			console.log(response.data.date);
+			console.log(response.data.title);
+		}
+	})
+	.catch((err) => {
+		console.error(err);
+	});
