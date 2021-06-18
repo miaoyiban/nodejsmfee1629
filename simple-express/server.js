a822e0e0622056d4b5e20fa9eec39aeaedec4ab7
kun <fdsa0872@gmail.com>
2021-06-18 15:10:43 +0800

    express static

--- simple-express/server.js
+++ simple-express/server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 let app = express();
 
+// 可以指定一個或多個目錄是靜態資源目錄
+// 自動幫你為public裡面的檔案建立路由
+app.use(express.static("public"))
+
 // 中間件 middleware
 app.use(function(req, res, next){
     let current = new Date();
     console.log(`有人來訪問了在${current}`)
+    // 讓他往下繼續
     next();
 })
+
+
 
 // 路由
 app.get("/",function(req,res){
@@ -17,6 +24,7 @@ app.get("/about", function (req, res) {
 });
 app.get("/test", function (req, res) {
 	res.send("test Express");
+    
 });
 
 app.listen(3000,()=>{
