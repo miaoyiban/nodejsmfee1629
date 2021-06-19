d35025d2e2a38f6e4d0854ec0fb64f2730bef1f4
kun <fdsa0872@gmail.com>
2021-06-19 16:17:07 +0800

    fetch版

--- simple-express/public/javascripts/api.js
+++ simple-express/public/javascripts/api.js
@@ -1,28 +1,37 @@
 // 用Ajax撈資料
-$(function(){
-    //  $.ajax({
+$(function () {
+	//  $.ajax({
 	// 			method: "GET",
-	// 			url: "/api/stocks",				
+	// 			url: "/api/stocks",
 	// 			dataType: "json",
 	// 		})
 	// 			.done(function (data) {
 	// 				console.log(data);
-					
+
 	// 			})
 	// 			.fail(function (error) {
 	// 				console.log(error);
 	// 			})
 	// 			.always(function () {});
-    axios
-			.get("/api/stocks")
-			.then(function (response) {
-				console.log(response.data);
-			})
-			.catch(function (error) {
-				// handle error
-				console.log(error);
-			})
-			.then(function () {
-				// always executed
-			});
-})
+	// axios
+	// 		.get("/api/stocks")
+	// 		.then(function (response) {
+	// 			console.log(response.data);
+	// 		})
+	// 		.catch(function (error) {
+	// 			// handle error
+	// 			console.log(error);
+	// 		})
+	// 		.then(function () {
+	// 			// always executed
+	// 		});
+
+	// fetch API 也是promise based
+	fetch("/api/stocks")
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (myJson) {
+			console.log(myJson);
+		});
+});
