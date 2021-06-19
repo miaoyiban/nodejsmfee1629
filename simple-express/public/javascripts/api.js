f34186ed76a79c30b50677472cd19093b2aba5c1
kun <fdsa0872@gmail.com>
2021-06-19 15:57:49 +0800

    axios版本

--- simple-express/public/javascripts/api.js
+++ simple-express/public/javascripts/api.js
@@ -1,16 +1,28 @@
 // 用Ajax撈資料
 $(function(){
-     $.ajax({
-				method: "GET",
-				url: "/api/stocks",				
-				dataType: "json",
-			})
-				.done(function (data) {
-					console.log(data);
+    //  $.ajax({
+	// 			method: "GET",
+	// 			url: "/api/stocks",				
+	// 			dataType: "json",
+	// 		})
+	// 			.done(function (data) {
+	// 				console.log(data);
 					
-				})
-				.fail(function (error) {
-					console.log(error);
-				})
-				.always(function () {});
+	// 			})
+	// 			.fail(function (error) {
+	// 				console.log(error);
+	// 			})
+	// 			.always(function () {});
+    axios
+			.get("/api/stocks")
+			.then(function (response) {
+				console.log(response);
+			})
+			.catch(function (error) {
+				// handle error
+				console.log(error);
+			})
+			.then(function () {
+				// always executed
+			});
 })
